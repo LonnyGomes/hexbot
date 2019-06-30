45940f656a41c30d293f4234fc08a27f4740904a
Lonny Gomes <code@lonnygomes.com>
2019-06-30 18:23:11 -0400

    Add base getGeoColors() function, #3
    Add error checking tests

--- src/index.controller.js
+++ src/index.controller.js
@@ -4,4 +4,35 @@ export class IndexController {
     constructor() {
         this.hexBot = new HexBot();
     }
+
+    /**
+     * Retrieves geo coordinate/color pairs
+     * @param {number} count total number of geocoords/colors to return, with a max of 1,000
+     * @returns {Array} array of color/geocord pairs
+     */
+    async getGeoColors(count) {
+        if (!count) {
+            count = 1000;
+        }
+
+        if (isNaN(count)) {
+            throw new Error('A number between 1-1,000 must be provided');
+        }
+
+        if (count < 1 || count > 1000) {
+            throw new Error(
+                'Invalid range provided. Numbers must be between 1 and 1000'
+            );
+        }
+
+        let geoColors = null;
+        try {
+            const { colors } = await this.hexBot.getColors(count);
+            geoColors = colors;
+        } catch (error) {
+            throw error;
+        }
+
+        return geoColors;
+    }
 }
--- tests/index.controller.test.js
+++ tests/index.controller.test.js
@@ -1,11 +1,81 @@
 import { IndexController } from '../src/index.controller';
 
+import { HexBot } from '../src/HexBot';
+jest.mock('../src/HexBot');
+
 describe('IndexController', () => {
     describe('constructor', () => {
         test('should instantiate HexBot', () => {
             const controller = new IndexController();
 
             expect(controller.hexBot).toBeDefined();
+        });
+    });
+
+    describe('getGeoColors()', () => {
+        beforeEach(() => {
+            HexBot.mockClear();
+        });
+
+        test('should throw error if a non-number is provided', async () => {
+            const controller = new IndexController();
+            const errMsg = /A number between/;
+
+            expect.assertions(2);
+            try {
+                await controller.getGeoColors('number');
+            } catch (e) {
+                expect(e.message).toMatch(errMsg);
+            }
+
+            try {
+                await controller.getGeoColors([1, 2, 3]);
+            } catch (e) {
+                expect(e.message).toMatch(errMsg);
+            }
+        });
+
+        test('should throw an error if number is out of bounds', async () => {
+            const controller = new IndexController();
+            const errMsg = /Invalid range provided/;
+
+            expect.assertions(2);
+            try {
+                await controller.getGeoColors(-1);
+            } catch (e) {
+                expect(e.message).toMatch(errMsg);
+            }
+
+            try {
+                await controller.getGeoColors(1001);
+            } catch (e) {
+                expect(e.message).toMatch(errMsg);
+            }
+        });
+
+        test('should retrieve colors/geo coordinate pairs', async () => {
+            const controller = new IndexController();
+
+            expect(HexBot.mock.instances.length).toEqual(1);
+            const hexBotInstance = HexBot.mock.instances[0];
+
+            hexBotInstance.getColors.mockResolvedValue({
+                colors: [
+                    { value: '#8CD7F3' },
+                    { value: '#1DF195' },
+                    { value: '#64BF06' },
+                    { value: '#7ED6DC' },
+                    { value: '#CE5220' },
+                    { value: '#C6736D' },
+                    { value: '#16E2AF' },
+                    { value: '#DDD5EF' },
+                    { value: '#FFC427' },
+                    { value: '#0069C6' }
+                ]
+            });
+
+            const geoColors = await controller.getGeoColors();
+            expect(geoColors.length).toEqual(10);
         });
     });
 });
