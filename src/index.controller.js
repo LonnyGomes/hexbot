15f06332e63545afca5cd83ee3780a565a37ccdd
Lonny Gomes <code@lonnygomes.com>
2019-06-30 09:56:51 -0400

    Create index controller to manage main functionality, #3

---
+++ src/index.controller.js
@@ -0,0 +1,7 @@
+import { HexBot } from './HexBot';
+
+export class IndexController {
+    constructor() {
+        this.hexBot = new HexBot();
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import css from './css/styles.css';
 import { CesiumController } from './cesium.controller';
+import { IndexController } from './index.controller';
 
+const controller = new IndexController();
 const cesium = new CesiumController();
 
 cesium.initViewer('cesiumContainer');
