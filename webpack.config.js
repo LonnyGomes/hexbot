9bf5d1d4912003cadbb62baf2acb47123d68b892
Lonny Gomes <code@lonnygomes.com>
2019-06-30 01:56:09 -0400

    Update webpack config/deps to work with cesium, #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
 const path = require('path');
+const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopywebpackPlugin = require('copy-webpack-plugin');
+
+// The path to the CesiumJS source code
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+
+        // Needed to compile multiline strings in Cesium
+        sourcePrefix: ''
+    },
+    amd: {
+        // Enable webpack-friendly use of require in Cesium
+        toUrlUndefined: true
+    },
+    node: {
+        // Resolve node module use of fs
+        fs: 'empty'
+    },
+    resolve: {
+        alias: {
+            // CesiumJS module name
+            cesium: path.resolve(__dirname, cesiumSource)
+        }
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -15,6 +39,20 @@ module.exports = {
         new HtmlWebpackPlugin({
             hash: true,
             template: './index.html' //relative to root of the application
+        }),
+        // Copy Cesium Assets, Widgets, and Workers to a static directory
+        new CopywebpackPlugin([
+            { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' }
+        ]),
+        new CopywebpackPlugin([
+            { from: path.join(cesiumSource, 'Assets'), to: 'Assets' }
+        ]),
+        new CopywebpackPlugin([
+            { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
+        ]),
+        new webpack.DefinePlugin({
+            // Define relative base path in cesium for loading assets
+            CESIUM_BASE_URL: JSON.stringify('')
         })
     ],
     module: {
@@ -32,6 +70,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+                use: ['url-loader']
             }
         ]
     }
