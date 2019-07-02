95b6e5fcd20bace8f74f05acd5e6c00ba5eb4f4d
Lonny Gomes <code@lonnygomes.com>
2019-07-01 20:39:17 -0400

    Tweak exosphere value, #5

--- src/HexBot.js
+++ src/HexBot.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const END_POINT = 'https://api.noopschallenge.com/hexbot';
-export const EXOSPHERE_DISTANCE = 10000000; // in meters
+export const EXOSPHERE_DISTANCE = 5000000; // in meters
 
 export class HexBot {
     constructor() {}
