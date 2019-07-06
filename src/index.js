2637b64accc6d8c451f84755e40fd4ef57bdc03e
Lonny Gomes <code@lonnygomes.com>
2019-07-06 17:36:26 -0400

    Add controls for total colors and to get new ones

--- src/index.js
+++ src/index.js
@@ -13,16 +13,47 @@ const viewer = cesium.initViewer('cesiumContainer', {
     timeline: false
 });
 
-const init = async () => {
+let timeoutId = null;
+
+// DOM references
+const btn = document.getElementById('clrBtn');
+const colorCount = document.getElementById('clrCount');
+const colorValLbl = document.getElementById('clrVal');
+
+const updateHandler = () => {
+    colorValLbl.value = `${colorCount.value} color${
+        colorCount.value > 1 ? 's' : ''
+    }`;
+    delayedGetColors();
+};
+colorCount.addEventListener('input', updateHandler);
+
+btn.addEventListener('click', () => {
+    updateGlobe(colorCount.value);
+});
+
+// updates globe with new set of random colors from the HexBot API
+const updateGlobe = async (count = 1000) => {
     try {
-        const geoColors = await controller.getGeoColors();
+        const geoColors = await controller.getGeoColors(count);
 
         dataSource.load(geoColors);
         dataSource.seriesToDisplay = 'defaultSeries';
+        viewer.dataSources.removeAll(true);
         viewer.dataSources.add(dataSource);
     } catch (error) {
         console.error('Error', error.message);
     }
 };
 
-init();
+// add simple debounce logic for input range
+const delayedGetColors = () => {
+    if (timeoutId) {
+        window.clearTimeout(timeoutId);
+    }
+
+    timeoutId = window.setTimeout(() => updateGlobe(colorCount.value), 500);
+};
+
+// invoke handler manually for the first time
+updateHandler();
